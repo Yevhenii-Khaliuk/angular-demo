e9f4ae0e8666737f49027c4b4c01b1bc58dbadf8
Yevhenii Khaliuk <yevhenii.khaliuk@gmail.com>
2019-04-12 13:17:27 +0300

    Fix issue with mixed styles

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {CategoryService} from '../category.service';
 import {Category} from '../model/category.model';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CategoriesComponent implements OnInit {
 
